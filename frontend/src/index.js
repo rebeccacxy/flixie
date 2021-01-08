2629a30d01cbba460d54d56af23a82fb97803bb2
rohit <rajeshbhatrohit@gmail.com>
2021-01-08 13:53:11 +0800

    Removed unnessasary pages and refactored page names

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,24 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
-
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+import LoginPage from "views/Login";
+import Dashboard from "views/Dashboard";
+// import CreateGroup from "views/CreateGroup";
+// import Preferences from "views/Preferences";
+// import Explore from "views/Explore";
+// import Likes from "views/Likes";
+// import Info from "views/Info";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/" component={Components} />
+      <Route path="/" component={LoginPage} />
+      <Route path="/dashboard" component={Dashboard} />
+      {/* <Route path="/create-group" component={CreateGroup} />
+      <Route path="/preferences" component={Preferences} />
+      <Route path="/explore" component={Explore} />
+      <Route path="/likes" component={Likes} />
+      <Route path="/info" component={Info} /> */}
     </Switch>
   </Router>,
   document.getElementById("root")
