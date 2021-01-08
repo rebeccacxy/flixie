bf2f30c22c58b328277c61598c5a9e838ce41619
rohit <rajeshbhatrohit@gmail.com>
2021-01-08 11:10:13 +0800

    Removed redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./components/App";
-import { store } from "./store/index";
-
+`g`
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
